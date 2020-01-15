9cbeca76192aed247bd8f5363180e2ca6e5bd800
rohit.yadav <rohit.yadav@NOHMSF3LGFWS116.noida.jkt.in>
2020-01-15 17:19:41 +0530

    5:00 evening

--- src/demo/abc.java
+++ src/demo/abc.java
@@ -4,7 +4,7 @@ public class abc {
 	public static void main(String[] args) {
 		String s =new String();
 		s="hello  India  23";
-		
+		String s1="hello  India  47";
 		System.out.println(s  );
 	}
 
