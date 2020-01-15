8c21493d366ed703447b40039ed1c05480e83ed2
rohit.yadav <rohit.yadav@NOHMSF3LGFWS116.noida.jkt.in>
2020-01-15 16:05:51 +0530

    new

--- src/demo/abc.java
+++ src/demo/abc.java
@@ -3,7 +3,7 @@ package demo;
 public class abc {
 	public static void main(String[] args) {
 		String s =new String();
-		s="hello  India";
+		s="hello  India  23";
 		
 		System.out.println(s  );
 	}
